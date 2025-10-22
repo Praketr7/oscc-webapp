1961d66dd333f3b481faa0ad6ec5cc68a9ddcd0f
Praket Reddy <praketrr7@gmail.com>
2025-10-23 02:12:51 +0530

    backend url

--- frontend/app/page.jsx
+++ frontend/app/page.jsx
@@ -11,7 +11,7 @@ export default function Page() {
     setLoading(true);
     setResult({nstage:"", ene:""});
     try {
-      const res = await fetch("http://127.0.0.1:5000/predict", {
+      const res = await fetch("https://oscc-webapp.onrender.com/predict", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(formData),
