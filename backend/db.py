7b411c09a696fa763e4a815b6e8167d3532d9518
Praket Reddy <praketrr7@gmail.com>
2025-10-23 01:23:04 +0530

    minor tweaks

--- backend/db.py
+++ backend/db.py
@@ -5,11 +5,11 @@ import pandas as pd
 
 load_dotenv()
 
-DB_HOST = os.getenv("DB_HOST", "localhost")
-DB_NAME = os.getenv("DB_NAME", "oscc_db")
-DB_USER = os.getenv("DB_USER", "postgres")
-DB_PASSWORD = os.getenv("DB_PASSWORD", "Barcelona@2025")
-DB_PORT = os.getenv("DB_PORT", 5432)
+DB_HOST = os.getenv("DB_HOST")
+DB_NAME = os.getenv("DB_NAME")
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_PORT = int(os.getenv("DB_PORT", 5432))  
 
 def get_connection():
     """Returns a psycopg2 connection suitable for pandas"""
