9637c746aeb1162310f81ea51286dc18699e6b89
Ananyaraj Shekhar <ananyarajs2006@gmail.com>
2025-10-22 14:46:27 +0530

    DB commit

--- backend/app.py
+++ backend/app.py
@@ -1,11 +1,13 @@
 from flask import Flask, request, jsonify
 import joblib
 from flask_cors import CORS
+from db import get_connection
+
 
 app = Flask(__name__)
 CORS(app)
-model = joblib.load("rusboost.pkl")
 
+model = joblib.load("rusboost.pkl")
 sex_mapping = {"Male":1, "Female":2}
 
 @app.route('/predict', methods=['POST'])
@@ -13,8 +15,29 @@ def predict():
     data = request.get_json()
     sex_input = sex_mapping.get(data['sex'])
     inputs = [data["age"], sex_input, data["sites"], data["doi"], data["tStage"], data["nlr"], data["pmr"], data["plr"], data["lmr"], data["sii"]]
+    
+    # Make prediction
     predictions = model.predict([inputs])
-    return jsonify({'prediction': int(predictions[0])})
+    nstage = int(predictions[0])
+    
+    # Save to DB
+    try:
+        conn = get_connection()
+        cursor = conn.cursor()
+        cursor.execute(
+            """
+            INSERT INTO oscc_table(age, sex, sites, doi, tstage, nlr, pmr, plr, lmr, sii, nstage)
+            VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)
+            """,
+            (data["age"], sex_input, data["sites"], data["doi"], data["tStage"], data["nlr"], data["pmr"], data["plr"], data["lmr"], data["sii"], nstage)
+        )
+        conn.commit()
+        cursor.close()
+        conn.close()
+    except Exception as e:
+        print("DB Error:", e)
+
+    return jsonify({'prediction': nstage})
 
 if __name__=='__main__':
     app.run(debug=True)
