330f1da9bbc4d6db51eaa8c8f35dc61cb4c3517c
Praket Reddy <praketrr7@gmail.com>
2025-10-24 15:31:20 +0530

    change

--- .next/static/chunks/src_app_page_jsx_e3e9cd31._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/src_36f4fd91._.js",
-  "static/chunks/node_modules_2580b763._.js"
-],
-    source: "dynamic"
-});
